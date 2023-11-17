bc97f100663deec7bbd575ab2f65b9884dd85649
Nima <nima@gardoon.io>
2023-11-17 23:36:14 +0330

    [feat] Header component created.

---
+++ src/Components/Layout/Header.jsx
@@ -0,0 +1,19 @@
+import mealImage from "../../Assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} />
+      </div>
+    </>
+  );
+};
+
+export default Header;
